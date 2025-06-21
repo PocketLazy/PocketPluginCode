5bda527d530b57188b80e2e9d77a4452b96c4d0a
PocketLazy <pocketpenguin.penguin@gmail.com>
2025-06-21 09:47:47 -0700

    Update CustomItems.java

--- pocketplugin/src/main/java/com/pocketlazy/pocketplugin/CustomItems.java
+++ pocketplugin/src/main/java/com/pocketlazy/pocketplugin/CustomItems.java
@@ -2,7 +2,5 @@ package com.pocketlazy.pocketplugin;
 
 import org.bukkit.event.Listener;
 
-// No more Wooden Chopper or Lumber Axe here.
-// You can still use this class for other unrelated custom items if needed.
 public class CustomItems implements Listener {
 }
